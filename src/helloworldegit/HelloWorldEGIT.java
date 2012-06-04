9543471451b211d605021c3249caec9d3c3dec0a
Imanol Bernabeu <bernabeu@carecm.de>
2012-06-04 18:22:55 +0200

    Test Commit

--- src/helloworldegit/HelloWorldEGIT.java
+++ src/helloworldegit/HelloWorldEGIT.java
@@ -5,4 +5,8 @@ public class HelloWorldEGIT {
 	public static void main( String[] args ){
 		System.out.println("Hello World from EGIT");
 	}
+	
+	public void test_one(){
+		System.out.println("ww");		
+	}
 }
